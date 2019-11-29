05f13716e9ce3823e06f13c405cb6a9825822723
xiaoyaor <747976016@qq.com>
2019-11-29 08:30:37 +0800

    删除多余Linux下报错代码

--- app/admin/controller/Addon.php
+++ app/admin/controller/Addon.php
@@ -31,7 +31,6 @@ class Addon extends Backend
      */
     public function index()
     {
-        require_once (root_path().'vendor\xiaoyaor\think-addons\src\helper.php');
         $addons = get_addon_list();
         foreach ($addons as $k => &$v) {
             $config = get_addon_config($v['name']);
