d7e21e21d34dfe586dfd3f182890cae6c422e0d7
xiaoyaor <747976016@qq.com>
2020-05-24 15:55:50 +0800

    upload

--- app/admin/controller/Ajax.php
+++ app/admin/controller/Ajax.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use app\common\controller\Backend;
 use think\addons\Service;
+use think\facade\Db;
 use think\facade\Cache;
 use think\facade\Lang;
 
@@ -35,6 +36,68 @@ class Ajax extends Backend
         //默认只加载了控制器对应的语言名，你还根据控制器名来加载额外的语言包
         $this->loadlang($controllername);
         return jsonp(Lang::get(), 200, [], ['json_encode_param' => JSON_FORCE_OBJECT | JSON_UNESCAPED_UNICODE]);
+    }
+
+    /**
+     * 通用排序
+     * @throws \think\db\exception\DbException
+     */
+    public function weigh()
+    {
+        //排序的数组
+        $ids = $this->request->post("ids");
+        //拖动的记录ID
+        $changeid = $this->request->post("changeid");
+        //操作字段
+        $field = $this->request->post("field");
+        //操作的数据表
+        $table = $this->request->post("table");
+        //主键
+        $pk = $this->request->post("pk");
+        //排序的方式
+        $orderway = $this->request->post("orderway", "", 'strtolower');
+        $orderway = $orderway == 'asc' ? 'ASC' : 'DESC';
+        $sour = $weighdata = [];
+        $ids = explode(',', $ids);
+        $prikey = $pk ? $pk : (Db::name($table)->getPk() ?: 'id');
+        $pid = $this->request->post("pid");
+        //限制更新的字段
+        $field = in_array($field, ['weigh']) ? $field : 'weigh';
+
+        // 如果设定了pid的值,此时只匹配满足条件的ID,其它忽略
+        if ($pid !== '') {
+            $hasids = [];
+            $list = Db::name($table)->where($prikey, 'in', $ids)->where('pid', 'in', $pid)->field("{$prikey},pid")->select();
+            foreach ($list as $k => $v) {
+                $hasids[] = $v[$prikey];
+            }
+            $ids = array_values(array_intersect($ids, $hasids));
+        }
+
+        $list = Db::name($table)->field("$prikey,$field")->where($prikey, 'in', $ids)->order($field, $orderway)->select();
+        foreach ($list as $k => $v) {
+            $sour[] = $v[$prikey];
+            $weighdata[$v[$prikey]] = $v[$field];
+        }
+        $position = array_search($changeid, $ids);
+        $desc_id = $sour[$position];    //移动到目标的ID值,取出所处改变前位置的值
+        $sour_id = $changeid;
+        $weighids = array();
+        $temp = array_values(array_diff_assoc($ids, $sour));
+        foreach ($temp as $m => $n) {
+            if ($n == $sour_id) {
+                $offset = $desc_id;
+            } else {
+                if ($sour_id == $temp[0]) {
+                    $offset = isset($temp[$m + 1]) ? $temp[$m + 1] : $sour_id;
+                } else {
+                    $offset = isset($temp[$m - 1]) ? $temp[$m - 1] : $sour_id;
+                }
+            }
+            $weighids[$n] = $weighdata[$offset];
+            Db::name($table)->where($prikey, $n)->update([$field => $weighdata[$offset]]);
+        }
+        $this->success();
     }
 
     /**
