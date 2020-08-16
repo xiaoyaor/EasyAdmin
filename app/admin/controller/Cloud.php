b272741b3bb5607b5c817bdc526f42c1f103fb2e
xiaoyaor <747976016@qq.com>
2020-08-16 10:46:47 +0800

    upload

---
+++ app/admin/controller/Cloud.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\common\controller\Backend;
+use think\facade\View;
+
+/**
+ *  云端平台
+ */
+class Cloud extends Backend
+{
+
+    //构造方法
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 查看
+     * @throws \Exception
+     */
+    public function index()
+    {
+        return '';
+    }
+
+}
