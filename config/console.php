c74a6e721bd4ca1d484f14af71d0f558fd6f05fe
xiaoyaor <747976016@qq.com>
2021-03-19 23:41:11 +0800

    调整配置文件和版本号

--- config/console.php
+++ config/console.php
@@ -5,11 +5,5 @@
 return [
     // 指令定义
     'commands' => [
-        'addons\command\app\admin\command\Crud',
-        'addons\command\app\admin\command\Menu',
-        'addons\command\app\admin\command\Install',
-        'addons\command\app\admin\command\Min',
-        'addons\command\app\admin\command\Addon',
-        'addons\command\app\admin\command\Api',
     ],
 ];
